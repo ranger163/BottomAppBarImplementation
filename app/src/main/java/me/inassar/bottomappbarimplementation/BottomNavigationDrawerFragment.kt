342fdd785aba62158d2b52e3f42b28d71cc740c1
Nassar163 <ahmed.nassar163@gmail.com>
2019-01-19 18:37:34 +0200

    Adding menu items to BottomAppBar Adding bottom navigation drawer Handling click listeners on Bottom navigation drawer Handling click listeners on menu items

--- app/src/main/java/me/inassar/bottomappbarimplementation/BottomNavigationDrawerFragment.kt
+++ app/src/main/java/me/inassar/bottomappbarimplementation/BottomNavigationDrawerFragment.kt
@@ -5,19 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
-import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.fragment_bottomsheet.*
 
 class BottomNavigationDrawerFragment : BottomSheetDialogFragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_bottomsheet, container, false)
-        navDrawerItemClick(view)
-        return view
+        return inflater.inflate(R.layout.fragment_bottomsheet, container, false)
     }
 
-    private fun navDrawerItemClick(view: View) {
-        val navView = view.findViewById(R.id.navigationView) as NavigationView
-        navView.setNavigationItemSelectedListener {
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        navigationView.setNavigationItemSelectedListener {
+            dismiss()
             when (it.itemId) {
                 R.id.nav1 -> context?.toast("${it.title} clicked")
                 R.id.nav2 -> context?.toast("${it.title} clicked")
