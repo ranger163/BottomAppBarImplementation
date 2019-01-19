8c6034c086effb9819a9b17f28a4fcb19421d882
Nassar163 <ahmed.nassar163@gmail.com>
2019-01-19 16:14:06 +0200

    Adding FAB to BottomAppBar Handling click listeners to BottomAppBar navigation button Handling click listeners to FAB

--- app/src/main/java/me/inassar/bottomappbarimplementation/MainActivity.kt
+++ app/src/main/java/me/inassar/bottomappbarimplementation/MainActivity.kt
@@ -1,13 +1,29 @@
 package me.inassar.bottomappbarimplementation
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var count = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        interactions()
+    }
+
+    private fun interactions() {
+        bottomAppBar.setNavigationOnClickListener {
+            Toast.makeText(this, "Navigation menu clicked", Toast.LENGTH_SHORT).show()
+        }
+        fab.setOnClickListener {
+            textTv.text = "$count"
+            count++
+        }
     }
 
 }
