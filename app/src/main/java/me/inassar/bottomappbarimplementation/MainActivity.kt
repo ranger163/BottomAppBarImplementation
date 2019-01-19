db1a7df63415727ac9ee8955fce140df4d262a60
Nassar163 <ahmed.nassar163@gmail.com>
2019-01-19 15:44:05 +0200

    Setting up material dependencies Changing app Theme to MaterialComponents Converting to androidX Implementing basic BottomAppBar

--- app/src/main/java/me/inassar/bottomappbarimplementation/MainActivity.kt
+++ app/src/main/java/me/inassar/bottomappbarimplementation/MainActivity.kt
@@ -1,7 +1,7 @@
 package me.inassar.bottomappbarimplementation
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +9,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
 }
